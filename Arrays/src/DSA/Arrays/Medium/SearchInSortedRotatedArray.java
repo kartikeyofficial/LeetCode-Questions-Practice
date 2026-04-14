7553aae6a842b1c9e276fff000a8b9b7ad50cc61
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-04-14 23:27:53 +0530

    Search in Rotated Sorted Array

--- Arrays/src/DSA/Arrays/Medium/SearchInSortedRotatedArray.java
+++ Arrays/src/DSA/Arrays/Medium/SearchInSortedRotatedArray.java
@@ -12,14 +12,14 @@ public class SearchInSortedRotatedArray {
                 return mid;
             }
             if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < mid) {
+                if (nums[left] <= target && target < nums[mid]) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
             } else {
-                if (nums[mid] < target && target <= right) {
-                    left = mid - 1;
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
                 } else {
                     right = mid - 1;
                 }
