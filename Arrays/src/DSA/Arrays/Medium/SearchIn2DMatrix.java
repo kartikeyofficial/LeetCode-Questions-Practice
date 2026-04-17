3d43f4f4436f8ebc6fec39da5290486d856e165c
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-04-18 00:04:44 +0530

    Seacrh Element in Sorted 2D Matrix

--- Arrays/src/DSA/Arrays/Medium/SearchIn2DMatrix.java
+++ Arrays/src/DSA/Arrays/Medium/SearchIn2DMatrix.java
@@ -1,9 +1,26 @@
 package DSA.Arrays.Medium;
 
+import java.util.Scanner;
+
 public class SearchIn2DMatrix {
     static void main(String[] args) {
-        int[][] matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
-        System.out.println(searchMatrix(matrix,27));
+        Scanner x = new Scanner(System.in);
+        System.out.print("Enter the Size of Length matrix: ");
+        int row = x.nextInt();
+        System.out.print("Enter the Size of Breadth matrix: ");
+        int cols = x.nextInt();
+        int[][] matrix = new int[row][cols];
+        for (int i=0;i<row;i++){
+            for (int j=0; j<cols;j++){
+                System.out.print("Enter the Matrix Element ("+(i+1)+","+(j+1)+"): ");
+                matrix[i][j] = x.nextInt();
+            }
+        }
+        System.out.print("Enter the Element do You Find int The Matrix: ");
+        int target = x.nextInt();
+
+//        int[][] matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        System.out.println(searchMatrix(matrix,target));
 
     }
     public static boolean searchMatrix(int[][] matrix, int target) {
